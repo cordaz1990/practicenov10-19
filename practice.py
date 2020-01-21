80bfeeb84e44cc6c93fd7d0dbf611bd62847db9f
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 20:53:08 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,19 @@
-print("hello")
-print("world")
-print("hello")
-print()
+def mergesort(L: list) -> None:
+    """  Reorder the items in L from smallest to largets.
+    L = [3,4,7,-1,2,5]
+    mergesort(L)
+    L
+    [-1,2,3,4,5,7]
+    """
+    for i in range(len(L)):
+      workspace.append([L[i]])
+      
+    i = 0
+    while i < len(workspace) - 1
+         L1 = workspace[i]
+         L2 = workspace[i + 1]
+         newL = merge(L1, L2)
+         workspace.append(newL)
+         i += 2
+   if len(workspace) != 0
+        L[:] = workspace[-1][:]
